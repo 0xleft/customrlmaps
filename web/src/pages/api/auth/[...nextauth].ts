f15b4e828b22f4930f4399f93dd5d4c56579ff8a
left <107749872+0xleft@users.noreply.github.com>
2024-03-07 17:02:54 +0000

    more

--- web/src/pages/api/auth/[...nextauth].ts
+++ web/src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,13 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "@/lib/auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
 
 export default NextAuth({
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
 })
