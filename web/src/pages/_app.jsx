06615dbcb14cab4331b1a42d4e98bc517eef76d7
left <107749872+0xleft@users.noreply.github.com>
2024-03-25 00:10:53 +0000

    Update _app.jsx

--- web/src/pages/_app.jsx
+++ web/src/pages/_app.jsx
@@ -2,11 +2,15 @@ import '@/styles/globals.css';
 
 import RootLayout from '@/components/Layout';
 import { SessionProvider } from 'next-auth/react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
 	return (
 		<SessionProvider session={pageProps.session}>
 			<RootLayout>
+				<Head>
+					<title>CustomRLMaps</title>
+				</Head>
 				<Component {...pageProps} />
 			</RootLayout>
 		</SessionProvider>
