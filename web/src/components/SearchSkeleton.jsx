349396ecaecf92b8d8811fb9e60d3a31ee8554d5
left <107749872+0xleft@users.noreply.github.com>
2024-04-17 20:06:26 +0100

    better search

--- web/src/components/SearchSkeleton.jsx
+++ web/src/components/SearchSkeleton.jsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
-import { Card, CardDescription, CardTitle } from "./ui/card";
+import { Card, CardContent, CardDescription, CardTitle } from "./ui/card";
 import { Skeleton } from "./ui/skeleton";
 import { useRouter } from "next/router";
 import { DownloadIcon, EyeOpenIcon } from "@radix-ui/react-icons";
 import { AspectRatio } from "./ui/aspect-ratio";
 import { getNiceNumber } from "@/utils/numberUtils";
+import { Badge } from "./ui/badge";
 
 function SingleSeachSkeleton() {
 	return (
 		<>
-			<div className="flex flex-col space-y-2 min-h-80 w-full">
+			<div className="flex flex-col space-y-4 w-full">
 				<Skeleton className="aspect-video" />
 				<Skeleton className="h-6 w-1/2" />
-				<div className="flex flex-row justify-between space-x-2">
+
+				<div className="flex flex-row justify-between space-x-4">
 					<Skeleton className="h-6 w-1/3" />
 					<Skeleton className="h-6 w-1/3" />
 				</div>
@@ -28,8 +30,8 @@ export function SearchItem({ project }) {
 
 	return (
 		<>
-			<Card className="flex flex-col space-y-2 min-h-80 w-full hover:shadow-lg transition hover:scale-105 duration-75">
-				<img src={project.imageUrl} className="hover:cursor-pointer w-full h-52 max-h-52 aspect-video" onClick={() => {
+			{/* <Card className="flex flex-col space-y-2 min-h-80 w-full hover:shadow-lg transition hover:scale-105 duration-75">
+				<img src={project.imageUrl} className="hover:cursor-pointer w-full max-h-52 aspect-video object-cover" onClick={() => {
 					router.push(`/project/${project.name}`);
 				}} />
 				
@@ -56,6 +58,40 @@ export function SearchItem({ project }) {
 							<DownloadIcon />
 							{getNiceNumber(project.downloads)} downloads
 						</p>
+					</div>
+				</div>
+			</Card> */}
+			<Card>
+				<div className="flex flex-col w-full">
+					<img src={project.imageUrl} className="hover:cursor-pointer w-full aspect-video object-cover rounded-md" onClick={() => {
+						router.push(`/project/${project.name}`);
+					}} />
+
+					<div className="p-2 space-y-2">
+						<h1 className="text-xl font-bold hover:cursor-pointer hover:underline"
+							onClick={() => {
+								router.push(`/project/${project.name}`);
+							}}
+						>{project.name}
+							<Badge className="ml-2">{project.type}</Badge>
+						</h1>
+						<CardDescription className="text-clip overflow-clip break-words pb-4 hover:cursor-pointer hover:underline"
+							onClick={() => {
+								router.push(`/user/${project.user}`);
+							}}
+						>
+							By @{project.user}
+						</CardDescription>
+						<div className="flex flex-row justify-between space-x-2 overflow-hidden">
+							<div className="h-6 w-full flex flex-row justify-center items-center">
+								<DownloadIcon className="w-4 h-4 inline" />
+								{getNiceNumber(project.downloads)}
+							</div>
+							<div className="h-6 w-full flex flex-row justify-center items-center">
+								<EyeOpenIcon className="w-4 h-4 inline" />
+								{getNiceNumber(project.views)}
+							</div>
+						</div>
 					</div>
 				</div>
 			</Card>
