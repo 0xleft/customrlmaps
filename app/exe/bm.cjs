fc3e55786d030a2a815845434d841d8b2ef49ace
left <107749872+0xleft@users.noreply.github.com>
2024-04-22 19:33:32 +0100

    Update bm.cjs

--- app/exe/bm.cjs
+++ app/exe/bm.cjs
@@ -3,14 +3,12 @@ const AppPath = global.AppPath;
 const axios = require('axios');
 
 function isBMInstalled() {
-    return fs.existsSync(`${AppPath}/portf.exe`) && fs.existsSync(`${AppPath}/bakkesmod.dll`) && fs.existsSync(`${AppPath}/pluginsdk.dll`);
+    return fs.existsSync(`${AppPath}/portf.exe`) && fs.existsSync(`${AppPath}/bakkesmod.zip`);
 }
 
 async function installBM() {
-    const response = await axios.get('https://github.com/pageuplt/bmdll/releases/latest/download/bakkesmod.dll', { responseType: 'arraybuffer' });
-    fs.writeFileSync(`${AppPath}/bakkesmod.dll`, Buffer.from(response.data));
-    const response2 = await axios.get('https://github.com/pageuplt/bmdll/releases/latest/download/pluginsdk.dll', { responseType: 'arraybuffer' });
-    fs.writeFileSync(`${AppPath}/pluginsdk.dll`, Buffer.from(response2.data));
+    const response2 = await axios.get('https://github.com/pageuplt/bmdll/releases/latest/download/bakkesmod.zip', { responseType: 'arraybuffer' });
+    fs.writeFileSync(`${AppPath}/bakkesmod.zip`, Buffer.from(response2.data));
     const response4 = await axios.get('https://github.com/pageuplt/bmdll/releases/latest/download/portf.exe', { responseType: 'arraybuffer' });
     fs.writeFileSync(`${AppPath}/portf.exe`, Buffer.from(response4.data));
     return true;
